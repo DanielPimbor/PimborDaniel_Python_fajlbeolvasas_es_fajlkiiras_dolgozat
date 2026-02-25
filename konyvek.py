629e63e564fa9110da85f176c9878b617dced3f8
DanielPimbor <pimbor.daniel@diak.szbi-pg.hu>
2026-02-25 09:22:28 +0100

    4-6. feladat

--- konyvek.py
+++ konyvek.py
@@ -24,9 +24,6 @@ with open('beolvasando_adatok/konyvek.txt', 'r', encoding='utf-8') as forrasfajl
                 'Kiadó': adatok[3]}
         konyvek.append(konyv)
 
-print(f'{konyvek=}')
-
-
 #1. feladat
 
 konyek_szama = len(konyvek)
@@ -44,41 +41,70 @@ for konyv in konyvek:
 #3. feladat
 
 legkevesebb_konyv_oldalszam = 0
-legkevesebb_konyv_oldalszam_konyv = ''
+legkevesebb_oldalszam_konyv = ''
 
 for konyv in konyvek:
-    if konyv['Oldalszám'] < legkevesebb_konyv_oldalszam:
+    if legkevesebb_konyv_oldalszam == 0:
         legkevesebb_konyv_oldalszam = konyv['Oldalszám']
-        legkevesebb_konyv_oldalszam_konyv = konyv['Cím']
+        legkevesebb_oldalszam_konyv = konyv['Cím']
+
+    elif konyv['Oldalszám'] < legkevesebb_konyv_oldalszam:
+        legkevesebb_konyv_oldalszam = konyv['Oldalszám']
+        legkevesebb_oldalszam_konyv = konyv['Cím']
 
 #4. feladat
 
-legtobb_konyv_iro = ''
-legtobb_iro_konyvei = []
-legtobb_konyv = 0
+legtobb_konyv=0
+legtobb_konyv_iroi = []
+
+konyvek_iroi=[]
 
 for konyv in konyvek:
-    if konyv['Szerző'] == legtobb_konyv_iro:
-        legtobb_iro_konyvei.append(konyv['Cím'])
-        legtobb_konyv = len(legtobb_iro_konyvei)
+    konyvek_iroi.append(konyv['Szerző'])
 
-    else:
-        if legtobb_konyv < kony
+for iro in konyvek_iroi:
+    if konyvek_iroi.count(iro) > legtobb_konyv:
+        legtobb_konyv = konyvek_iroi.count(iro)
+        legtobb_konyv_iroi.append(iro)
+    elif konyvek_iroi.count(iro) == legtobb_konyv:
+        legtobb_konyv_iroi.append(iro)
+
+legtobb_konyv_iroi = set(legtobb_konyv_iroi)
+
+#5. feladat
+
+osszes_oldal = 0
+
+for konyv in konyvek:
+    osszes_oldal += konyv['Oldalszám']
+
+atlag = osszes_oldal / len(konyvek)
 
 
+#6. feladat
+
+legtobb_konyv_kiadonkent=0
+legtobb_konyv_kiadoja = []
+
+konyvek_kiadoi=[]
+
+for konyv in konyvek:
+    konyvek_kiadoi.append(konyv['Kiadó'])
+
+for kiado in konyvek_kiadoi:
+    if konyvek_kiadoi.count(kiado) > legtobb_konyv_kiadonkent:
+        legtobb_konyv_kiadonkent = konyvek_iroi.count(kiado)
+        legtobb_konyv_kiadoja.append(kiado)
+
+    elif konyvek_iroi.count(kiado) == legtobb_konyv_kiadonkent:
+        legtobb_konyv_kiadoja.append(kiado)
+
+legtobb_konyv_kiadoja = set(legtobb_konyv_kiadoja)
 
 
-
-
-
-
-
-
-
-
-print(f"1. A beolvasott fájlban összesen ____ könyv szerepel.")
-print(f"2. A legtöbb oldalas könyv: ____")
-print(f"3. A legkevesebb oldalas könyv: ____")
-print(f"4. A legtöbb könyvet író szerző: ____")
-print(f"5. Az átlagos oldalszám: ____")
-print(f"***A legtöbb könyvet kiadó kiadó: ____")
+print(f"1. A beolvasott fájlban összesen {konyek_szama} könyv szerepel.")
+print(f"2. A legtöbb oldalas könyv: {legtobb_konyv_oldalszam_konyv }")
+print(f"3. A legkevesebb oldalas könyv: {legkevesebb_oldalszam_konyv} ")
+print(f"4. A legtöbb könyvet író szerző: {legtobb_konyv_iroi}")
+print(f"5. Az átlagos oldalszám: {atlag:.2f}")
+print(f"***A legtöbb könyvet kiadó kiadó: {legtobb_konyv_kiadoja}")
